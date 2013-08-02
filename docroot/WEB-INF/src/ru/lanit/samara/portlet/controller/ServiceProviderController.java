7e45ab6412958eb3fa3524e6cdfe441796e13700
galimru <galimovrf@gmail.com>
2013-08-03 02:19:03 +0400

    add controller

--- docroot/WEB-INF/src/ru/lanit/samara/portlet/controller/ServiceProviderController.java
+++ docroot/WEB-INF/src/ru/lanit/samara/portlet/controller/ServiceProviderController.java
@@ -1,14 +1,14 @@
-package ru.lanit.samara.portlet.action;
+package ru.lanit.samara.portlet.controller;
 
 import com.liferay.portal.kernel.portlet.ConfigurationAction;
+import com.liferay.util.bridges.mvc.MVCPortlet;
 
 import javax.portlet.*;
 
 /**
  * Created by Galimov Ruslan
  */
-public class ConfigurationActionImpl implements ConfigurationAction {
-
+public class ServiceProviderController extends MVCPortlet implements ConfigurationAction {
     @Override
     public void processAction(PortletConfig portletConfig, ActionRequest request, ActionResponse response) throws Exception {
         //To change body of implemented methods use File | Settings | File Templates.
