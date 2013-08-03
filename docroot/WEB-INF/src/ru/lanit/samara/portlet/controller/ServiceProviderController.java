3197eac8ffba4fced46e66beefe740589b92f586
galimru <galimovrf@gmail.com>
2013-08-03 13:24:33 +0400

    add hello world example

--- docroot/WEB-INF/src/ru/lanit/samara/portlet/controller/ServiceProviderController.java
+++ docroot/WEB-INF/src/ru/lanit/samara/portlet/controller/ServiceProviderController.java
@@ -1,14 +1,22 @@
 package ru.lanit.samara.portlet.controller;
 
 import com.liferay.portal.kernel.portlet.ConfigurationAction;
+import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.util.bridges.mvc.MVCPortlet;
 
 import javax.portlet.*;
+import java.io.IOException;
 
 /**
  * Created by Galimov Ruslan
  */
 public class ServiceProviderController extends MVCPortlet implements ConfigurationAction {
+
+    @Override
+    public void doView(RenderRequest renderRequest, RenderResponse renderResponse) throws IOException, PortletException {
+        super.doView(renderRequest, renderResponse);
+    }
+
     @Override
     public void processAction(PortletConfig portletConfig, ActionRequest request, ActionResponse response) throws Exception {
         //To change body of implemented methods use File | Settings | File Templates.
@@ -17,5 +25,10 @@ public class ServiceProviderController extends MVCPortlet implements Configurati
     @Override
     public String render(PortletConfig portletConfig, RenderRequest renderRequest, RenderResponse renderResponse) throws Exception {
         return "/html/config.jsp";
+    }
+
+    public void sendHello(ActionRequest request, ActionResponse response) {
+        String username = ParamUtil.get(request, "username", "empty");
+        request.setAttribute("username", username);
     }
 }
--- docroot/html/view.jsp
+++ docroot/html/view.jsp
@@ -1,5 +1,31 @@
 <%@ taglib uri="http://java.sun.com/portlet_2_0" prefix="portlet" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 
 <portlet:defineObjects />
 
-This is the <b>Service Provider Portlet</b> portlet.
+<portlet:actionURL name="sendHello" var="sendHelloURL"/>
+
+<c:if test="${not empty username}">
+<div>Hello, ${username}!</div>
+</c:if>
+
+<form action="<%=sendHelloURL.toString()%>" method="post">
+    <input type="text" name="username"/>
+    <input type="submit" value="send"/>
+</form>
+
+<script type="text/javascript">
+    $(function() {
+        $( "#accordion" ).accordion({
+            active: false,
+            collapsible: true
+        });
+    });
+</script>
+
+<div id="accordion">
+    <h3>First header</h3>
+    <div>First content</div>
+    <h3>Second header</h3>
+    <div>Second content</div>
+</div>
